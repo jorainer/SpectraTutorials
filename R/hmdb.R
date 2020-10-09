d881e6624022f30773ee5f018bb51689218fdf5e
jorainer <johannes.rainer@gmail.com>
2020-10-09 15:18:49 +0200

    Add documentation of the hmdb object

---
+++ R/hmdb.R
@@ -0,0 +1,13 @@
+#' @title MS/MS Spectra from the Human Metabolome Database
+#'
+#' @description
+#'
+#' `hmdb` is a [Spectra()] object with MS/MS spectra from the Human Metabolome
+#' Database [HMDB](https://hmdb.ca). The object was built from xml files
+#' downloaded from the HMDB web site on August 3 2020.
+#'
+#' Please read the licensing information on the
+#' [HMDB download page](https://hmdb.ca/downloads) for usage of these data.
+#'
+#' @name hmdb
+NULL
