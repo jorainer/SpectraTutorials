04ee698f67b141289b2a6a7ae68d4a3c3eb07d5f
jorainer <johannes.rainer@gmail.com>
2021-02-26 08:37:35 +0100

    docs: use 2021.02 MassBank release and expand description

---
+++ NEWS.md
@@ -0,0 +1,5 @@
+# Version 0.3
+
+## Version 0.3.2
+
+- Use MassBank release 2021.02 and add expand descriptive text.
--- scripts/mysql-prepare.sql
+++ scripts/mysql-prepare.sql
@@ -1,6 +1,6 @@
 create database MassBank character set = 'utf8';
 
-create user 'massbank'@'%';
+create user 'massbank'@'%' identified by 'massbank';
 grant select on MassBank.* to 'massbank'@'%';
 grant show view on MassBank.* to 'massbank'@'%';
 flush privileges;
