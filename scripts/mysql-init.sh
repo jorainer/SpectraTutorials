25cd9c84f7cf1614fa7226788a001f0cc8cd0bad
jorainer <johannes.rainer@gmail.com>
2023-03-14 13:48:59 +0100

    ci: fix build and deploy script

--- scripts/mysql-init.sh
+++ scripts/mysql-init.sh
@@ -1,14 +1,33 @@
-## Set up MySQL init scripts
-## mkdir -p /etc/services.d/mysql
+## For documentation etc also check:
+## https://github.com/rocker-org/rocker-versioned/issues/219
+
+# ## Set up MySQL init scripts
+# ## mkdir -p /etc/services.d/mysql
+# # echo '#!/usr/bin/with-contenv bash
+# # ## load /etc/environment vars first:
+# # for line in $( cat /etc/environment ) ; do export $line > /dev/null; done
+# # exec /usr/bin/mysqld_safe 0' \
+# echo '#!/usr/bin/with-contenv sh
+# exec /usr/bin/mysqld_safe --user=mysql --skip-syslog --no-auto-restart --nowatch' \
+# > /etc/cont-init.d/mysql
+# # > /etc/services.d/mysql/run
+
+# # echo '#!/bin/bash
+# # service mysql stop' \
+# # > /etc/services.d/mysql/finish
+
+# ## Same with rstudio start.
+# mkdir -p /etc/services.d/mysql
 # echo '#!/usr/bin/with-contenv bash
-# ## load /etc/environment vars first:
-# for line in $( cat /etc/environment ) ; do export $line > /dev/null; done
-# exec /usr/bin/mysqld_safe 0' \
-echo '#!/usr/bin/with-contenv sh
-exec /usr/bin/mysqld_safe --user=mysql --skip-syslog --no-auto-restart --nowatch' \
-> /etc/cont-init.d/mysql
+# exec /usr/bin/mysqld_safe --user=mysql --skip-syslog --no-auto-restart --nowatch' \
 # > /etc/services.d/mysql/run
 
 # echo '#!/bin/bash
-# service mysql stop' \
+# service mariadb stop' \
 # > /etc/services.d/mysql/finish
+
+echo '#!/usr/bin/with-contenv sh
+exec /usr/bin/mysqld_safe --user=mysql --skip-syslog --no-auto-restart --nowatch' \
+> /etc/cont-init.d/03_mysql
+
+chmod a+x /etc/cont-init.d/03_mysql
